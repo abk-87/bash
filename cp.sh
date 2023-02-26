43863f6c0414018ba376f4e149c870a47fabd413
Babken Abreyan <babkenabreyan@gmail.com>
2023-02-26 21:27:30 +0400

    Edited cp.sh & mv.sh

--- cp.sh
+++ cp.sh
@@ -6,7 +6,7 @@ function file_cp()
 	cat "$1" > "$2"
 }
 
-#function for copying directories
+#function for copying a directory to other directory
 function dir_cp()
 {
 	local dirname=$(basename "$1")
@@ -22,6 +22,24 @@ function dir_cp()
 			dir_cp "$i" "$2/$dirname"
 		fi
 	done
+}
+
+#function for copying a directory under a different name
+function dir_cp2()
+{
+        local dirname=$(basename "$1")
+        mkdir "$2"
+        for i in "$1"/*
+        do
+                if [ -f "$i" ]
+                then
+                        local filename=$(basename "$i")
+                        file_cp "$i" "$2/$filename"
+                elif [ -d "$i" ]
+                then
+                        dir_cp2 "$i" "$2/$(basename "$i")"
+                fi
+        done
 }
 
 #function for copying files and directories to the directory
@@ -42,9 +60,14 @@ function cp_to_dir()
 if [ "$#" -eq 2 -a -f "$1" -a ! -d "$2" ]
 then
 	file_cp "$1" "$2"
-elif [ "$#" -eq 2 -a -d "$1" -a -d "$2" ]
+elif [ "$#" -eq 2 -a -d "$1" ]
 then
-	dir_cp "$1" "$2"
+	if [ -d "$2" ]
+        then
+                dir_cp "$1" "$2"
+        else
+                dir_cp2 "$1" "$2"
+        fi
 elif [ "$#" -ge 2 -a -d "${!#}" ]
 then
 	cp_to_dir $@
--- mv.sh
+++ mv.sh
@@ -1,21 +1,87 @@
 #$/bin/bash
 
-if [ "$#" -eq 2 -a -f "$1" ]
-then
+function file_mv()
+{	
 	if [ -f "$2" ]
 	then
 		rm "$2"
 	fi
-	ln "$1" "$2"
-	rm "$1"
-elif [ "$#" -ge 2 -a -d "${!#}" ]
-then
+	if [ -d "$2" ]
+	then
+		ln "$1" "$2" 2>/dev/null || cat "$1" > "$2/$(basename "$1")"
+	else
+		ln "$1" "$2" 2>/dev/null || cat "$1" > "$2"
+	fi
+	if [ -f "$1" ]
+	then
+		rm "$1"
+	fi	
+}
+
+function dir_mv()
+{
+	local dirname=$(basename "$1")
+        mkdir "$2/$dirname"
+        for i in "$1"/*
+        do
+                if [ -f "$i" ]
+                then
+                        local filename=$(basename "$i")
+			file_mv "$i" "$2/$dirname/$filename"
+                elif [ -d "$i" ]
+                then
+                        dir_mv "$i" "$2/$dirname"
+                fi
+        done
+}
+
+function dir_mv2()
+{
+	local dirname=$(basename "$1")
+        mkdir "$2"
+        for i in "$1"/*
+        do
+                if [ -f "$i" ]
+                then
+                        local filename=$(basename "$i")
+			file_mv "$i" "$2/$filename"
+                elif [ -d "$i" ]
+                then
+			dir_mv2 "$i" "$2/$(basename "$i")"
+                fi
+        done
+}
+
+function mv_to_dir()
+{
 	for i in "$@"
 	do
 		if [ -f "$i" ]
 		then
-			ln "$i" "${!#}"
-			rm "$i"
+			file_mv "$i" "${!#}"
+		elif [ -d "$i" -a "$i" != "${!#}" ]
+		then
+			dir_mv "$i" "${!#}"
+			rm -r "$i"
 		fi
 	done
+}
+
+
+if [ "$#" -eq 2 -a -f "$1" ]
+then
+	file_mv "$1" "$2"
+elif [ "$#" -eq 2 -a -d "$1" ]
+then
+	if [ -d "$2" ]
+	then
+		dir_mv "$1" "$2"
+		rm -r "$1"
+	else
+		dir_mv2 "$1" "$2"
+		rm -r "$1"
+	fi
+elif [ "$#" -gt 2 -a -d "${!#}" ]
+then
+	mv_to_dir "$@"
 fi
